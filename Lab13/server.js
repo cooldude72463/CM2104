39f3875177e3940053a2d8b221289725a6a0c5bb
KIERAN SMOLLET (1604402) <1604402@rgu.ac.uk>
2018-03-15 10:18:36 +0000

    h

--- Lab13/server.js
+++ Lab13/server.js
@@ -15,4 +15,10 @@ app.get('/oneLineJoke', function(req, res){
   res.end(randomJoke.body);
 })
 
+app.get('/add', function(req, res){
+  var x = parseInt(req.query.x);
+  var y = parseInt(req.query.y);
+  res.send("X + Y="+(x+y));
+});
+
 app.listen(8080);
