cbeb184686ac70a0eabe6d6f1955efb1301bbe56
KIERAN SMOLLET (1604402) <1604402@rgu.ac.uk>
2018-03-15 10:15:48 +0000

    h

--- Lab13/server.js
+++ Lab13/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var oneLinerJoke = require('one-liner-joke');
 
 app.get('/', function(req, res){
   res.send("Hello world! by express");
@@ -7,6 +8,11 @@ app.get('/', function(req, res){
 
 app.get('/test', function(req, res){
   res.send("This is route 2");
+})
+
+app.get('/oneLineJoke', function(req, res){
+  var randomJoke = oneLinerJoke.getRandomJoke();
+  res.end(randomJoke.body);
 })
 
 app.listen(8080);
